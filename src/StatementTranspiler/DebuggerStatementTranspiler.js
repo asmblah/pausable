49dde8b96fe9b47a1e4892db1b9a0ce23612e711
Daniel Phillimore <dan@ovms.co>
2015-08-22 23:18:38 +0100

    Added support for debugger statements to the transpiler

---
+++ src/StatementTranspiler/DebuggerStatementTranspiler.js
@@ -0,0 +1,34 @@
+/*
+ * Pausable - Pause and resume JavaScript code
+ * Copyright (c) Dan Phillimore (asmblah)
+ * http://asmblah.github.com/pausable/
+ *
+ * Released under the MIT license
+ * https://github.com/asmblah/pausable/raw/master/MIT-LICENSE.txt
+ */
+
+'use strict';
+
+var _ = require('lodash'),
+    acorn = require('acorn'),
+    estraverse = require('estraverse'),
+    Syntax = estraverse.Syntax;
+
+function DebuggerStatementTranspiler(statementTranspiler, expressionTranspiler) {
+    this.expressionTranspiler = expressionTranspiler;
+    this.statementTranspiler = statementTranspiler;
+}
+
+_.extend(DebuggerStatementTranspiler.prototype, {
+    getNodeType: function () {
+        return Syntax.DebuggerStatement;
+    },
+
+    transpile: function (node, parent, functionContext, blockContext) {
+        blockContext.prepareStatement().assign({
+            'type': Syntax.DebuggerStatement
+        });
+    }
+});
+
+module.exports = DebuggerStatementTranspiler;
